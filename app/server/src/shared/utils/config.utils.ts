89cc4dfa62d3e7ce8c6479d7f00c37d98359c8bc
pagoru <pagoru@gmail.com>
2024-06-30 01:16:17 +0200

    fix #9

--- app/server/src/shared/utils/config.utils.ts
+++ app/server/src/shared/utils/config.utils.ts
@@ -1,13 +1,16 @@
-import { readYaml } from "./yaml.utils.ts";
+import { readYaml, writeYaml } from "./yaml.utils.ts";
 import { ConfigTypes } from "shared/types/config.types.ts";
 import { CONFIG_DEFAULT } from "shared/consts/config.consts.ts";
 
 export const getConfig = async (): Promise<ConfigTypes> => {
-  const config = await readYaml<ConfigTypes>("./config.yml");
+  let config;
+  try {
+    config = await readYaml<ConfigTypes>("./config.yml");
+  } catch (e) {}
 
-  return {
-    name: config.name || CONFIG_DEFAULT.name,
-    description: config.description || CONFIG_DEFAULT.description,
+  const defaults: ConfigTypes = {
+    name: config?.name || CONFIG_DEFAULT.name,
+    description: config?.description || CONFIG_DEFAULT.description,
     limits: {
       players: config?.limits?.players || CONFIG_DEFAULT.limits.players,
       handshake: config?.limits?.handshake || CONFIG_DEFAULT.limits.handshake,
@@ -17,4 +20,9 @@ export const getConfig = async (): Promise<ConfigTypes> => {
       server: config?.ports?.server || CONFIG_DEFAULT.ports.server,
     },
   };
+  try {
+    await writeYaml<ConfigTypes>("./config.yml", defaults);
+  } catch (e) {}
+
+  return defaults;
 };
