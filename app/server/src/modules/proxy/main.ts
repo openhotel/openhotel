de267ba28dcae67906b477cbbb57e64e3487fe1a
pagoru <pagoru@gmail.com>
2024-07-01 18:22:24 +0200

    wip #27

--- app/server/src/modules/proxy/main.ts
+++ app/server/src/modules/proxy/main.ts
@@ -2,6 +2,7 @@ import { ConfigTypes, ModuleProps } from "shared/types/main.ts";
 import { getClientSocket, getServerSocket } from "socket_ionic";
 import { getParentWorker } from "worker_ionic";
 import {
+  debug,
   getFreePort,
   getRandomString,
   initLog,
@@ -46,59 +47,63 @@ export const load = async (args: ModuleProps, config: ConfigTypes) => {
 
   const firewallsServer = getServerSocket(args.internal.proxyPort);
 
-  // firewallsServer.on(
-  //   "guest",
-  //   (clientId: string, [clientToken]) =>
-  //     !firewallClient && clientToken === args.internal.token,
-  // );
-  // firewallsServer.on("connected", (client) => {
-  //   firewallClient = client;
-  //
-  //   onReady();
-  //   serverClient.emit("ready");
-  //
-  //   firewallClient.on("open", async ({ username, workerId, userId }) => {
-  //     const workerPort = await getFreePort();
-  //     const workerToken = getRandomString(16);
-  //
-  //     proxyClientWorkerMap[userId] = getParentWorker({
-  //       url: new URL(
-  //         "../../shared/workers/proxy-client.worker.ts",
-  //         import.meta.url,
-  //       ).href,
-  //     });
-  //
-  //     proxyClientWorkerMap[userId].on("joined", () => {
-  //       serverClient.emit("joined", { userId, username });
-  //     });
-  //     proxyClientWorkerMap[userId].on("disconnected", () => {
-  //       serverClient.emit("left", { userId, username });
-  //
-  //       firewallClient.emit("close", { userId, username });
-  //
-  //       proxyClientWorkerMap[userId].close();
-  //       delete proxyClientWorkerMap[userId];
-  //     });
-  //
-  //     proxyClientWorkerMap[userId].on("data", ({ event, message }) => {
-  //       serverClient.emit("data", { event, message, userId, username });
-  //     });
-  //
-  //     const data = {
-  //       userId,
-  //       workerId,
-  //       username,
-  //       port: workerPort,
-  //       token: workerToken,
-  //     };
-  //
-  //     // We start listening on the worker
-  //     proxyClientWorkerMap[userId].emit("start", data);
-  //     // We inform firewall
-  //     firewallClient.emit("open", data);
-  //   });
-  // });
-  // firewallsServer.on("disconnected", (client) => {
-  //   onDisconnected("firewall");
-  // });
+  firewallsServer.on(
+    "guest",
+    (clientId: string, [clientToken]) =>
+      !firewallClient && clientToken === args.internal.token,
+  );
+  firewallsServer.on("connected", (client) => {
+    firewallClient = client;
+
+    onReady();
+    serverClient.emit("ready");
+
+    firewallClient.on("open", async ({ username, workerId, userId }) => {
+      const workerPort = await getFreePort();
+      const workerToken = getRandomString(16);
+
+      proxyClientWorkerMap[userId] = getParentWorker({
+        url: new URL(
+          "../../shared/workers/proxy-client.worker.ts",
+          import.meta.url,
+        ).href,
+      });
+
+      proxyClientWorkerMap[userId].on("joined", () => {
+        serverClient.emit("joined", { userId, username });
+      });
+      proxyClientWorkerMap[userId].on("disconnected", () => {
+        serverClient.emit("left", { userId, username });
+
+        firewallClient.emit("close", { userId, username });
+
+        proxyClientWorkerMap[userId].close();
+        delete proxyClientWorkerMap[userId];
+      });
+
+      proxyClientWorkerMap[userId].on("data", ({ event, message }) => {
+        serverClient.emit("data", { event, message, userId, username });
+      });
+
+      const data = {
+        userId,
+        workerId,
+        username,
+        port: workerPort,
+        token: workerToken,
+      };
+
+      // We start listening on the worker
+      proxyClientWorkerMap[userId].emit("start", data);
+      // We inform firewall
+      firewallClient.emit("open", data);
+    });
+  });
+  firewallsServer.on("error", (error) => {
+    debug("error", error);
+  });
+  firewallsServer.on("disconnected", (error) => {
+    debug("disconnected", error);
+    onDisconnected("firewall");
+  });
 };
