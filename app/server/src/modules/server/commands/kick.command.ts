2a01f9cc411e89fd2ba583b7bd62f7cedac42ba0
Pablo González Rubio <pagoru@gmail.com>
2024-07-27 03:04:20 +0200

    fix #152 (#154)

---
+++ app/server/src/modules/server/commands/kick.command.ts
@@ -0,0 +1,15 @@
+import { Server } from "modules/server/main.ts";
+import { ProxyEvent } from "shared/enums/event.enum.ts";
+
+export const kickCommand = {
+  command: "kick",
+  func: async ({ args }) => {
+    const username = args[0];
+    if (!username) return;
+
+    const user = Server.users.get({ username });
+    if (!user) return;
+
+    Server.proxy.$emit(ProxyEvent.$DISCONNECT, { clientId: user.clientId });
+  },
+};
