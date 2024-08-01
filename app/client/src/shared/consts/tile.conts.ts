fae19e34a388459b59c91aa99c225e351ef347a2
pagoru <pagoru@gmail.com>
2024-08-02 00:26:34 +0200

    fix: fixed stairs height - fix #200

--- app/client/src/shared/consts/tile.conts.ts
+++ app/client/src/shared/consts/tile.conts.ts
@@ -5,6 +5,7 @@ export const TILE_SIZE: Size = {
   height: 25,
 };
 
+export const STEP_TILE_HEIGHT = 6;
 export const TILE_WIDTH = 12;
-export const TILE_Y_HEIGHT = TILE_WIDTH * 1.5;
+export const TILE_Y_HEIGHT = STEP_TILE_HEIGHT * 4;
 export const MOVEMENT_BETWEEN_TILES_DURATION = 500;
