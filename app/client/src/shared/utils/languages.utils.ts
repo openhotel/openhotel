7aeff59cc51bd455f805d970f83705141d985917
Alberto Quiles Boix <alberto.quil3s@gmail.com>
2024-08-02 01:31:09 +0200

    feat: support multi languages - fix #79

--- app/client/src/shared/utils/languages.utils.ts
+++ app/client/src/shared/utils/languages.utils.ts
@@ -8,8 +8,8 @@ let locale;
 export const loadLocale = async (code?: string) => {
   try {
     const $code = code ?? getBrowserLanguage();
-    const language = $code.split("-")[1] ?? "en";
-    const response = await fetch(`./locales/${language.toLowerCase()}.json`);
+    const language = $code.split("-")[0] ?? "en";
+    const response = await fetch(`./locales/${language}.json`);
     locale = await response.json();
   } catch (e) {
     console.error(e);
