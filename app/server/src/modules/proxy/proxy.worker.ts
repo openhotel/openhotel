64a5180e1ffbd8835712d557edf971b2da64ded6
Pablo González Rubio <pagoru@gmail.com>
2024-08-02 00:12:30 +0200

    fix: Prevent proxy crashing - fix #201 (#202)

--- app/server/src/modules/proxy/proxy.worker.ts
+++ app/server/src/modules/proxy/proxy.worker.ts
@@ -32,33 +32,53 @@ type DataEvent = {
 serverWorker.on(
   ProxyEvent.$USER_DATA,
   ({ users, event, message }: DataEvent) => {
-    // broadcast
-    if (users.includes("*")) return server.emit(event, message);
-    //
-    for (const user of users.map((userId) =>
-      userList.find((user) => user.id === userId),
-    ))
-      userClientMap[user?.clientId]?.emit(event, message);
+    try {
+      // broadcast
+      if (users.includes("*")) return server.emit(event, message);
+      //
+      for (const user of users.map((userId) =>
+        userList.find((user) => user.id === userId),
+      ))
+        userClientMap?.[user?.clientId]?.emit?.(event, message);
+    } catch (e) {
+      console.error(e);
+    }
   },
 );
 
 serverWorker.on(ProxyEvent.$ADD_ROOM, ({ roomId, userId }) => {
-  const user = userList.find((user) => user.id === userId);
-  if (!user) return;
-  server.getRoom(roomId).addClient(user.clientId);
+  try {
+    const user = userList.find((user) => user.id === userId);
+    if (!user) return;
+    server?.getRoom?.(roomId)?.addClient?.(user?.clientId);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 serverWorker.on(ProxyEvent.$REMOVE_ROOM, ({ roomId, userId }) => {
-  const user = userList.find((user) => user.id === userId);
-  if (!user) return;
-  server.getRoom(roomId).removeClient(user.clientId);
+  try {
+    const user = userList.find((user) => user.id === userId);
+    if (!user) return;
+    server?.getRoom?.(roomId)?.removeClient?.(user?.clientId);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 serverWorker.on(ProxyEvent.$ROOM_DATA, ({ roomId, event, message }) => {
-  server.getRoom(roomId).emit(event, message);
+  try {
+    server?.getRoom?.(roomId)?.emit?.(event, message);
+  } catch (e) {
+    console.error(e);
+  }
 });
 serverWorker.on(ProxyEvent.$DISCONNECT_USER, ({ clientId }) => {
-  userClientMap[clientId]?.close();
+  try {
+    userClientMap?.[clientId]?.close?.();
+  } catch (e) {
+    console.error(e);
+  }
 });
 serverWorker.on(ProxyEvent.$UPDATE, async () => {
   const canUpdate = await loadUpdater({ config: $config, envs: $envs });
@@ -93,7 +113,7 @@ serverWorker.on("start", async ({ config, envs }: WorkerProps) => {
 
     setTimeout(() => {
       userList = userList.filter(
-        (user) => !(!user.clientId && user.session === session),
+        (user) => !(!user?.clientId && user?.session === session),
       );
       firewallWorker.emit("userList", { userList });
     }, 5_000);
@@ -107,7 +127,7 @@ serverWorker.on("start", async ({ config, envs }: WorkerProps) => {
   server.on("guest", (clientId: string, [token, session]) => {
     if (token !== protocolToken && userList.length >= config.limits.players)
       return false;
-    const foundUser = userList.find((user) => user.session === session);
+    const foundUser = userList.find((user) => user?.session === session);
     if (!foundUser) return false;
 
     // Disconnects user if already connected
@@ -118,47 +138,65 @@ serverWorker.on("start", async ({ config, envs }: WorkerProps) => {
     return true;
   });
   server.on("connected", async (client: ServerClient) => {
-    const foundUser = userList.find((user) => user.clientId === client.id);
-    if (!foundUser) return client.close();
+    try {
+      const foundUser = userList.find((user) => user.clientId === client.id);
+      if (!foundUser) return client?.close?.();
 
-    // Wait if current user is connected to be disconnected
-    await waitUntil(
-      () => !Object.values(clientIdUserIdMap).includes(foundUser.id),
-    );
-    // Assign the userId to the clientId. UserId can only be once as value.
-    clientIdUserIdMap[client.id] = foundUser.id;
+      // Wait if current user is connected to be disconnected
+      await waitUntil(
+        () => !Object.values(clientIdUserIdMap).includes(foundUser.id),
+      );
+      // Assign the userId to the clientId. UserId can only be once as value.
+      clientIdUserIdMap[client?.id] = foundUser.id;
 
-    userClientMap[foundUser.clientId] = client;
-    serverWorker.emit(ProxyEvent.$USER_JOINED, {
-      data: { user: foundUser },
-    });
-
-    client.on(ProxyEvent.$USER_DATA, ({ event, message }) => {
-      // Disconnect client if tries to send events outside the whitelist
-      if (!PROXY_CLIENT_EVENT_WHITELIST.includes(event)) return client.close();
-      serverWorker.emit(ProxyEvent.$USER_DATA, {
-        user: foundUser,
-        event,
-        message,
+      userClientMap[foundUser.clientId] = client;
+      serverWorker.emit(ProxyEvent.$USER_JOINED, {
+        data: { user: foundUser },
       });
-    });
-    client.emit(ProxyEvent.WELCOME, {
-      datetime: Date.now(),
-      user: foundUser,
-    });
+
+      client.on(ProxyEvent.$USER_DATA, ({ event, message }) => {
+        try {
+          // Disconnect client if tries to send events outside the whitelist
+          if (!PROXY_CLIENT_EVENT_WHITELIST.includes(event))
+            return client?.close?.();
+          serverWorker.emit(ProxyEvent.$USER_DATA, {
+            user: foundUser,
+            event,
+            message,
+          });
+        } catch (e) {
+          console.error(e);
+        }
+      });
+      client.emit(ProxyEvent.WELCOME, {
+        datetime: Date.now(),
+        user: foundUser,
+      });
+    } catch (e) {
+      console.error(e);
+    }
   });
   server.on("disconnected", (client: ServerClient) => {
-    const userId = clientIdUserIdMap[client.id];
+    try {
+      if (!client?.id) return;
 
-    const foundUser = userList.find((user) => user.id === userId);
+      const userId = clientIdUserIdMap[client.id];
+      if (!userId) return;
 
-    delete userClientMap[client.id];
-    delete clientIdUserIdMap[client.id];
+      const foundUser = userList.find((user) => user.id === userId);
 
-    userList = userList.filter((user) => user.clientId !== client.id);
-    firewallWorker.emit("userList", { userList });
+      if (!foundUser) return;
 
-    serverWorker.emit(ProxyEvent.$USER_LEFT, { data: { user: foundUser } });
+      delete userClientMap[client.id];
+      delete clientIdUserIdMap[client.id];
+
+      userList = userList.filter((user) => user.clientId !== client.id);
+      firewallWorker.emit("userList", { userList });
+
+      serverWorker.emit(ProxyEvent.$USER_LEFT, { data: { user: foundUser } });
+    } catch (e) {
+      console.error(e);
+    }
   });
   log(`Proxy started on :${config.proxy.port}`);
 });
