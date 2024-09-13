eaf097ba4e79e29ac1885a3e2ca0823e8fd7d05c
Pablo González Rubio <pagoru@gmail.com>
2024-09-13 21:58:43 +0200

    fix: cursor pointer inside private rooms modal - fix #287 (#288)

--- app/client/src/modules/interfaces/navigator/private-rooms/categories/own-category.component.ts
+++ app/client/src/modules/interfaces/navigator/private-rooms/categories/own-category.component.ts
@@ -1,20 +1,34 @@
-import { container, textSprite } from "@tu/tulip";
+import { container, EventMode, nineSliceSprite, textSprite } from "@tu/tulip";
 import { SpriteSheetEnum } from "shared/enums";
 
 export const ownCategoryComponent = () => {
-  const $container = container({
+  const $container = container();
+  const $content = container({
     position: {
-      x: 4,
-      y: 19,
+      x: 6,
+      y: 7,
     },
   });
+
+  const $backgroundModal = nineSliceSprite({
+    spriteSheet: SpriteSheetEnum.UI,
+    texture: "modal-2",
+    leftWidth: 6,
+    topHeight: 6,
+    rightWidth: 6,
+    bottomHeight: 6,
+    width: 201,
+    height: 100,
+    eventMode: EventMode.STATIC,
+  });
+  $container.add($backgroundModal, $content);
 
   const text = textSprite({
     spriteSheet: SpriteSheetEnum.DEFAULT_FONT,
     text: "my own",
     tint: 0,
   });
-  $container.add(text);
+  $content.add(text);
 
   return $container.getComponent(ownCategoryComponent);
 };
--- app/client/src/modules/interfaces/navigator/private-rooms/categories/top-category.component.ts
+++ app/client/src/modules/interfaces/navigator/private-rooms/categories/top-category.component.ts
@@ -1,20 +1,34 @@
-import { container, textSprite } from "@tu/tulip";
+import { container, EventMode, nineSliceSprite, textSprite } from "@tu/tulip";
 import { SpriteSheetEnum } from "shared/enums";
 
 export const topCategoryComponent = () => {
-  const $container = container({
+  const $container = container();
+  const $content = container({
     position: {
-      x: 4,
-      y: 19,
+      x: 6,
+      y: 7,
     },
   });
+
+  const $backgroundModal = nineSliceSprite({
+    spriteSheet: SpriteSheetEnum.UI,
+    texture: "modal-2",
+    leftWidth: 6,
+    topHeight: 6,
+    rightWidth: 6,
+    bottomHeight: 6,
+    width: 201,
+    height: 190,
+    eventMode: EventMode.STATIC,
+  });
+  $container.add($backgroundModal, $content);
 
   const text = textSprite({
     spriteSheet: SpriteSheetEnum.DEFAULT_FONT,
     text: "top",
     tint: 0,
   });
-  $container.add(text);
+  $content.add(text);
 
   return $container.getComponent(topCategoryComponent);
 };
