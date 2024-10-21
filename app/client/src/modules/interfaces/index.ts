dd9cc8ae820f26f5ae8573bad8b70d7a408cda74
Pablo González Rubio <pagoru@gmail.com>
2024-10-21 10:07:24 +0200

    feat: added latitude getter - fix #364 (#365)

--- app/client/src/modules/interfaces/index.ts
+++ app/client/src/modules/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from "./hot-bar-chat.component";
 export * from "./hot-bar.component";
 export * from "./hot-bar-items.component";
+export * from "./info.component";
 
 export * from "./navigator";
