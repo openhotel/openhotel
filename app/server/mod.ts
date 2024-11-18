91663a24fcbca2ef8fb4ad5f0f40d50adf2b3eac
Pablo González Rubio <pagoru@gmail.com>
2024-11-18 21:06:55 +0100

    feat: add spanish, european and more popular pride flags - fix #395 (#396)

--- app/server/mod.ts
+++ app/server/mod.ts
@@ -3,7 +3,7 @@ import { getProcessedEnvs } from "shared/utils/main.ts";
 import { System } from "modules/system/main.ts";
 
 const envs = getProcessedEnvs({
-  version: "v1.0.0",
+  version: "development",
 });
 
 await loadEnv();
