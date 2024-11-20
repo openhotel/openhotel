afe607be9fa534d6dc85891148ea01761aa092b3
kahsez <kahsez@users.noreply.github.com>
2024-11-20 18:32:24 +0100

    feat: write contributors as a json instead of a markdown. Select only users and not bots.

--- app/server/writeContributors.ts
+++ app/server/writeContributors.ts
@@ -1,6 +1,8 @@
 interface Contributor {
   login: string;
   html_url: string;
+  avatar_url: string;
+  type: string;
   contributions: number;
 }
 
@@ -13,25 +15,23 @@ async function fetchContributors() {
   )
     .then((response) => response.json())
     .then((response) => {
-      return response as Contributor[];
+      return response.map((contributor: Contributor) => ({
+        login: contributor.login,
+        html_url: contributor.html_url,
+        avatar_url: contributor.avatar_url,
+        type: contributor.type,
+        contributions: contributor.contributions,
+      }));
     });
 }
 
-async function getFormattedContributors() {
-  const contributors = await fetchContributors();
-  let contributorsMarkdown = "# Contributors\n";
-  contributors.forEach((contributor: Contributor) => {
-    contributorsMarkdown += `- [${contributor.login}](${contributor.html_url})\n`;
-  });
-  return contributorsMarkdown;
-}
-
-async function main() {
-  const defaultPath = "./build/CONTRIBUTORS.md";
+async function writeContributors() {
+  const defaultPath = "./build/contributors.json";
   const path = Deno.args.length === 0 ? defaultPath : Deno.args[0];
 
-  const contributors = await getFormattedContributors();
-  await Deno.writeTextFile(path, contributors);
+  const contributors: Contributor[] = await fetchContributors();
+  const humanContributors = contributors.filter((c) => c.type !== "Bot");
+  await Deno.writeTextFile(path, JSON.stringify(humanContributors, null, 2));
 }
 
-await main();
+await writeContributors();
