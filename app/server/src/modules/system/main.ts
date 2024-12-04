f4b029da94e024f23d06481aa3bb37e6f017328b
Alberto Quiles Boix <alberto.quil3s@gmail.com>
2024-12-04 21:14:52 +0100

    feat: implemented migrations - fix #46

--- app/server/src/modules/system/main.ts
+++ app/server/src/modules/system/main.ts
@@ -4,9 +4,10 @@ import { game } from "./game/main.ts";
 import { debug, initLog, log } from "shared/utils/main.ts";
 import { tasks } from "./tasks.ts";
 import { CONFIG_DEFAULT } from "shared/consts/config.consts.ts";
-import { getConfig, update, getDb } from "@oh/utils";
+import { getConfig, update, getDb, DbMutable } from "@oh/utils";
 import { onet } from "./onet/main.ts";
 import { auth } from "modules/shared/auth.ts";
+import { Migrations } from "../migrations/main.ts";
 
 export const System = (() => {
   let $config: ConfigTypes;
@@ -15,7 +16,7 @@ export const System = (() => {
   const $proxy = proxy();
   const $tasks = tasks();
   const $game = game();
-  const $db = getDb({ pathname: `./server-database` });
+  const $db: DbMutable = getDb({ pathname: `./server-database` });
   const $onet = onet();
   const $auth = auth();
 
@@ -67,6 +68,7 @@ export const System = (() => {
     await $auth.load($config, true);
     $proxy.load();
     await $db.load();
+    await Migrations.load($db);
     await $game.load();
     $tasks.load();
     await $onet.load();
