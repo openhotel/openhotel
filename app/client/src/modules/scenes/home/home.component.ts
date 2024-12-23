28624f65504e7094d1bf763057512027b9f8a9c5
Sergio Navarro <33727996+serNAVARRO7@users.noreply.github.com>
2024-12-23 23:35:40 +0100

    feat: display current online users (#486)
    Co-authored-by: pagoru <pagoru@gmail.com>

--- app/client/src/modules/scenes/home/home.component.ts
+++ app/client/src/modules/scenes/home/home.component.ts
@@ -9,10 +9,11 @@ import {
   graphics,
   GraphicType,
   Env,
+  textSprite,
 } from "@tu/tulip";
 import { logoComponent } from "modules/main";
 import { System } from "system";
-import { SystemEvent, TextureEnum } from "shared/enums";
+import { SystemEvent, TextureEnum, SpriteSheetEnum } from "shared/enums";
 import { hotBarComponent } from "modules/interfaces";
 import { BLACK_BAR_HEIGHT } from "shared/consts";
 import { wait } from "shared/utils";
@@ -42,6 +43,38 @@ export const homeComponent: ContainerComponent<Props> = () => {
   const $hotBar = hotBarComponent();
   $container.add($logo, $hotBar);
 
+  const onlineUsers = textSprite({
+    text: "0 guests online",
+    spriteSheet: SpriteSheetEnum.DEFAULT_FONT,
+    color: 0xffffff,
+    position: {
+      x: 0,
+      y: 18,
+    },
+    backgroundPadding: {
+      top: 2,
+      right: 4,
+      bottom: 2,
+      left: 4,
+    },
+    backgroundAlpha: 0.5,
+    backgroundColor: 0,
+    visible: false,
+  });
+  const $rePositionOnlineUsers = (size: Size) => {
+    onlineUsers.setPositionX(
+      size.width / 2 - onlineUsers.getBounds().width / 2,
+    );
+  };
+
+  const loadUsersOnline = async () => {
+    const { count } = await System.api.fetch<{
+      count: number;
+    }>("/online-users");
+    onlineUsers.setText(`${count} guest${count === 1 ? "" : "s"} online`);
+  };
+  $container.add(onlineUsers);
+
   const background = sprite({
     texture: TextureEnum.HOTEL_ALPHA_V1,
     zIndex: -1,
@@ -54,21 +87,32 @@ export const homeComponent: ContainerComponent<Props> = () => {
       y: backgroundBounds.height / 2 - size.height / 2,
     });
     upperBar.setRectangle(size.width, height + 2);
+    $rePositionOnlineUsers(size);
   };
-  $rePositionBackground(global.getApplication().window.getBounds());
   $container.add(background);
 
   let $removeOnResize;
+  let reloadInterval;
   $container.on(DisplayObjectEvent.MOUNT, async (e) => {
     $removeOnResize = global.events.on(Event.RESIZE, $rePositionBackground);
 
-    if (System.config.get().version !== "development") await wait(1250);
+    await loadUsersOnline();
+
+    reloadInterval = setInterval(() => {
+      if (!$container.isMounted()) return;
+      loadUsersOnline();
+    }, 30_000);
+
+    if (System.config.get().version !== "development") await wait(1_250);
     if (!$container.isMounted()) return;
 
+    onlineUsers.setVisible(true);
+    $rePositionBackground(global.getApplication().window.getBounds());
     System.events.emit(SystemEvent.SHOW_NAVIGATOR_MODAL);
   });
   $container.on(DisplayObjectEvent.UNMOUNT, (e) => {
     $removeOnResize();
+    clearInterval(reloadInterval);
     System.events.emit(SystemEvent.HIDE_NAVIGATOR_MODAL);
   });
 
