690b599e8fa7810e15ee09130f9fcd4922ff9455
Diego González Liarte <diegogliarte@gmail.com>
2025-01-23 17:06:07 +0100

    update discord invite

--- app/client/src/shared/consts/socials.consts.ts
+++ app/client/src/shared/consts/socials.consts.ts
@@ -8,7 +8,7 @@ type SocialData = {
 export const SOCIALS_LIST: Record<Socials, SocialData> = {
   [Socials.DISCORD]: {
     texture: "discord-white",
-    url: "https://discord.gg/7Jm3v2Q",
+    url: "https://discord.com/invite/qBZfPdNWUj",
   },
   [Socials.GITHUB]: {
     texture: "github-white",
