afff13f2300288e5b249ac28a5b9f9d3094029a8
pagoru <pagoru@gmail.com>
2025-01-24 23:43:52 +0100

    wip

--- app/server/src/modules/migrations/main.ts
+++ app/server/src/modules/migrations/main.ts
@@ -4,9 +4,9 @@ const MIGRATION_LIST = [
   await import("./2024-12-04--20-50-remove-me.migration.ts"),
   await import("./2024-12-11--23-00-add-rooms.migration.ts"),
   await import("./2024-12-16--01-03-remove-furniture-uid.migration.ts"),
-  await import("./2025-01-20--13-28-add-private-rooms.migration.ts"),
   await import("./2025-01-24--09-00-add-demo-room.migration.ts"),
   await import("./2025-02-14--00-20-change-demo-room-description.migration.ts"),
+  await import("./2025-01-24--23-32-add-private-rooms.migration.ts"),
 ];
 
 export const Migrations = (() => {
