f82d891ce2e3da188ee0091a61fb1d7dfd8d464c
pagoru <pagoru@gmail.com>
2025-02-04 11:28:47 +0100

    wip

--- app/server/src/modules/proxy/router/main.ts
+++ app/server/src/modules/proxy/router/main.ts
@@ -1,5 +1,14 @@
 import { getRequestRequest } from "./request.request.ts";
 import { getApiRequest } from "./api.request.ts";
-import { getInfoRequest } from "modules/proxy/router/info.request.ts";
+import { getInfoRequest } from "./info.request.ts";
+import { getIconRequest } from "./icon.request.ts";
+import { getBackgroundRequest } from "./background.request.ts";
 
-export const routesList = [getRequestRequest, getApiRequest, getInfoRequest];
+export const routesList = [
+  getRequestRequest,
+  getApiRequest,
+  getInfoRequest,
+  //
+  getIconRequest,
+  getBackgroundRequest,
+];
