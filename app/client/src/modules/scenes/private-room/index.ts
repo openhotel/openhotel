6cb4d064b7ee69e136191fbb770b13001279e434
pablo <pagoru@gmail.com>
2025-02-16 01:17:29 +0100

    wip

--- app/client/src/modules/scenes/private-room/index.ts
+++ app/client/src/modules/scenes/private-room/index.ts
@@ -3,5 +3,7 @@ export * from "./preview.component";
 export * from "./room.component";
 export * from "./wall.component";
 export * from "./private-room.component";
+export * from "./room-info.component";
+export * from "./hot-bar-chat.component";
 
 export * from "./furniture";
