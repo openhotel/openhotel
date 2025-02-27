4b9e655575c2d44d79412ff0f08ba19155b5e7dc
Alberto Quiles Boix <alberto.quil3s@gmail.com>
2025-02-27 17:06:35 +0100

    bump: @oh/utils - fix #683 (#684)

--- app/server/src/modules/migrations/2025-02-14--00-20-change-demo-room-description.migration.ts
+++ app/server/src/modules/migrations/2025-02-14--00-20-change-demo-room-description.migration.ts
@@ -4,7 +4,8 @@ export default {
   id: "2025-02-14--00-20-change-demo-room-description",
   description: "Change demo room description",
   up: async (db: DbMutable) => {
-    for (const { key, value } of await db.list({ prefix: ["rooms"] })) {
+    const { items: rooms } = await db.list({ prefix: ["rooms"] });
+    for (const { key, value } of rooms) {
       if (value.description === "This is a room for show all furnitures") {
         await db.set(key, {
           ...value,
@@ -15,7 +16,8 @@ export default {
     }
   },
   down: async (db: DbMutable) => {
-    for (const { key, value } of await db.list({ prefix: ["rooms"] })) {
+    const { items: rooms } = await db.list({ prefix: ["rooms"] });
+    for (const { key, value } of rooms) {
       if (value.description === "This is a room to show all furnitures") {
         await db.set(key, {
           ...value,
