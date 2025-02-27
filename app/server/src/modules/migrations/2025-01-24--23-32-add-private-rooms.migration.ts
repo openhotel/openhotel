627f99f77c355a19cb3428c2dc161b3e75853848
pagoru <pagoru@gmail.com>
2025-02-27 23:18:50 +0100

    wip

--- app/server/src/modules/migrations/2025-01-24--23-32-add-private-rooms.migration.ts
+++ app/server/src/modules/migrations/2025-01-24--23-32-add-private-rooms.migration.ts
@@ -4,7 +4,8 @@ export default {
   id: "2025-01-24--23-32-add-private-rooms",
   description: "Add private rooms to different table + max users",
   up: async (db: DbMutable) => {
-    for (const { key, value } of await db.list({ prefix: ["rooms"] })) {
+    const { items } = await db.list({ prefix: ["rooms"] });
+    for (const { key, value } of items) {
       await db.set(["rooms", "private", value.id], {
         ...value,
         maxUsers: 10,
@@ -13,9 +14,10 @@ export default {
     }
   },
   down: async (db: DbMutable) => {
-    for (const { key, value } of await db.list({
+    const { items } = await db.list({
       prefix: ["rooms", "private"],
-    })) {
+    });
+    for (const { key, value } of items) {
       delete value.maxUsers;
       await db.set(["rooms", value.id], value);
       await db.delete(key);
