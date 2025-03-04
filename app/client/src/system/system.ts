384fa74388ae08f3f5fc885496d2ed811791d448
pagoru <pagoru@gmail.com>
2025-03-04 19:04:43 +0100

    more

--- app/client/src/system/system.ts
+++ app/client/src/system/system.ts
@@ -1,9 +1,18 @@
 import { account } from "system/account";
+import { tasks } from "system/tasks";
 
 export const System = (() => {
+  const $tasks = tasks();
   const $account = account();
 
+  const load = () => {
+    $tasks.load();
+  };
+
   return {
+    tasks: $tasks,
     account: $account,
+
+    load,
   };
 })();
