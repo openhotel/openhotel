c6d8c90569a5e2f6f3e4539ad3bc7bbc64f5749d
pagoru <pagoru@gmail.com>
2025-03-13 22:09:34 +0100

    more

--- app/client/src/shared/components/index.ts
+++ app/client/src/shared/components/index.ts
@@ -2,3 +2,5 @@ export * from "./text";
 export * from "./loader";
 export * from "./loader-assets";
 export * from "./hot-bar-items";
+export * from "./private-room";
+export * from "./loading-bar";
