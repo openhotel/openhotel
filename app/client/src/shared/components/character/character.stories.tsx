277463e8a1a99bb96968c36a998556553c9200dc
pagoru <pagoru@gmail.com>
2025-03-14 15:39:15 +0100

    more

--- app/client/src/shared/components/character/character.stories.tsx
+++ app/client/src/shared/components/character/character.stories.tsx
@@ -27,18 +27,16 @@ export default {
 type Story = StoryObj<typeof CharacterComponent>;
 
 export const Character: Story = () => {
-  const [bodyDirection, setBodyDirection] = useState<Direction>(
-    Direction.NORTH,
-  );
+  const [bodyDirection, setBodyDirection] = useState<Direction>(Direction.EAST);
   const [headDirection, setHeadDirection] = useState<Direction>(null);
   const [bodyAnimation, setBodyAnimation] = useState<CharacterBodyAction>(
-    CharacterBodyAction.SIT,
+    CharacterBodyAction.IDLE,
   );
   const [leftArmAction, setLeftArmAction] = useState<CharacterArmAction>(
     CharacterArmAction.ARM_WAVE_0,
   );
   const [rightArmAction, setRightArmAction] = useState<CharacterArmAction>(
-    CharacterArmAction.IDLE,
+    CharacterArmAction.ARM_WAVE_0,
   );
 
   useEffect(() => {
