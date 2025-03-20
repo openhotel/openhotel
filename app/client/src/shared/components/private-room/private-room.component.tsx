33d53590fa53423b49835563fcc6a0709b7c1693
pagoru <pagoru@gmail.com>
2025-03-20 23:58:29 +0100

    more

--- app/client/src/shared/components/private-room/private-room.component.tsx
+++ app/client/src/shared/components/private-room/private-room.component.tsx
@@ -75,7 +75,6 @@ export const PrivateRoomComponent: React.FC<Props> = ({
             <PrivateRoomTile
               key={`tile${x}.${z}`}
               spawn={spawn}
-              alpha={0.5}
               position={{ x, y, z }}
               onPointerDown={() => onPointerTile?.({ x, y, z })}
               onPointerEnter={() =>
