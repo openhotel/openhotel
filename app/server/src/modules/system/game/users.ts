d0f1451c6e632e6ac77aaa592099cba4605a3454
Miguel <46541558+mmoya26@users.noreply.github.com>
2025-03-24 06:10:03 -0500

    fix: reset users last message - fix #679 (#697)
    Co-authored-by: pagoru <pagoru@gmail.com>

--- app/server/src/modules/system/game/users.ts
+++ app/server/src/modules/system/game/users.ts
@@ -107,6 +107,9 @@ export const users = () => {
 
     const moveToRoom = async (roomId: string) => {
       const currentRoom = getRoom();
+
+      setLastMessage(null);
+
       if (currentRoom) {
         const room = await System.game.rooms.get(currentRoom);
         room?.removeUser?.(getObject(), true);
@@ -140,10 +143,11 @@ export const users = () => {
     const getPathfinding = (): Point3d[] =>
       System.game.rooms.pathfinding.get(getAccountId()) || [];
 
-    const setLastMessage = (message: string) => {
+    const setLastMessage = (message: string | null) => {
       $userLastMessageMap[user.accountId] = message;
     };
-    const getLastMessage = (): string => $userLastMessageMap[user.accountId];
+    const getLastMessage = (): string | null =>
+      $userLastMessageMap[user.accountId];
 
     const setLastWhisper = (whisperUser: UserMutable | null) => {
       $userLastWhisperMap[user.accountId] = whisperUser
