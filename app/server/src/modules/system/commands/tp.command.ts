621cd0ed06d8bde445d8b716f78088ba28e47d4b
Fernando Maldonado <f_maldonado6@hotmail.com>
2025-03-26 16:11:15 -0600

    Simplified position object

--- app/server/src/modules/system/commands/tp.command.ts
+++ app/server/src/modules/system/commands/tp.command.ts
@@ -3,27 +3,27 @@ import { ProxyEvent } from "shared/enums/event.enum.ts";
 import { System } from "modules/system/main.ts";
 
 export const tpCommand: Command = {
-  command: "tp",
-  role: CommandRoles.OP,
-  usages: ["<x> <z>"],
-  description: "command.tp.description",
-  func: async ({ user, args }) => {
-    if (args.length !== 2) return;
+	command: "tp",
+	role: CommandRoles.OP,
+	usages: ["<x> <z>"],
+	description: "command.tp.description",
+	func: async ({ user, args }) => {
+		if (args.length !== 2) return;
 
-    const [x, z] = args as number[];
+		const [x, z] = args as number[];
 
-    if (isNaN(x) || isNaN(z)) return;
+		if (isNaN(x) || isNaN(z)) return;
 
-    const roomId = user.getRoom();
-    if (!roomId) return;
-    const room = await System.game.rooms.get(roomId);
-    const y = room.getYFromPoint({ x, z }) ?? 0;
+		const roomId = user.getRoom();
+		if (!roomId) return;
+		const room = await System.game.rooms.get(roomId);
+		const y = room.getYFromPoint({ x, z }) ?? 0;
 
-    user.setPosition({ x, z });
+		user.setPosition({ x, z });
 
-    room?.emit(ProxyEvent.SET_POSITION_HUMAN, {
-      accountId: user.getAccountId(),
-      position: { x, z, y: y },
-    });
-  },
+		room?.emit(ProxyEvent.SET_POSITION_HUMAN, {
+			accountId: user.getAccountId(),
+			position: { x, z, y },
+		});
+	},
 };
