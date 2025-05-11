bba5e1d466076043089f73ba40198d7c96545852
Alberto Quiles Boix <alberto.quil3s@gmail.com>
2025-05-12 01:42:39 +0200

    wip

--- app/client/src/shared/hooks/games/games.context.tsx
+++ app/client/src/shared/hooks/games/games.context.tsx
@@ -4,7 +4,6 @@ import { GameModule } from "@oh/utils";
 export interface GamesContextType {
   loadGame: (gameId: string) => Promise<GameModule | null>;
   unloadGame: (gameId: string) => void;
-  activeGames: Record<string, GameModule>;
 }
 
 export const GamesContext = createContext<GamesContextType>(undefined);
--- app/client/src/shared/hooks/games/games.provider.tsx
+++ app/client/src/shared/hooks/games/games.provider.tsx
@@ -1,47 +1,24 @@
-import React, { useCallback, useState } from "react";
-import { useApi } from "shared/hooks";
-import { GameModule, GameManifest } from "@oh/utils";
+import React, { useCallback } from "react";
+import { gamesLoaders, useApi } from "shared/hooks";
+import { GameModule } from "@oh/utils";
 import { GamesContext } from "./games.context";
 
 export const GamesProvider = ({ children }) => {
   const { fetch } = useApi();
 
-  // const [loadedModules, setLoadedModules] = useState<
-  //   Record<string, GameModule>
-  // >({});
-  const [activeGames, setActiveGames] = useState<Record<string, GameModule>>(
-    {},
-  );
-
   const loadGame = useCallback(
     async (gameId: string): Promise<GameModule | null> => {
-      // TODO: cause render loop
-      // if (loadedModules[gameId]) {
-      //   setActiveGames((prev) => ({
-      //     ...prev,
-      //     [gameId]: loadedModules[gameId],
-      //   }));
-      //   return loadedModules[gameId];
-      // }
-
       try {
-        const { manifest } = (await fetch(`/games/${gameId}`)) as {
-          manifest: GameManifest;
-        };
-        if (!manifest) {
-          console.warn(`Game manifest not found for ${gameId}`);
-          return null;
-        }
+        // const { manifest } = (await fetch(`/games/${gameId}`)) as {
+        //   manifest: GameManifest;
+        // };
+        // if (!manifest) {
+        //   console.warn(`Game manifest not found for ${gameId}`);
+        //   return null;
+        // }
 
-        const { game } = (await import(
-          /* @vite-ignore */ manifest.clientUrl
-        )) as { game: GameModule };
-
-        console.log(game);
-
-        // TODO: cause render loop
-        // setLoadedModules((prev) => ({ ...prev, [gameId]: game }));
-        // setActiveGames((prev) => ({ ...prev, [gameId]: game }));
+        const loader = gamesLoaders[gameId];
+        const { game } = await loader();
 
         return game;
       } catch (error) {
@@ -52,23 +29,12 @@ export const GamesProvider = ({ children }) => {
     [fetch],
   );
 
-  const unloadGame = useCallback(
-    (gameId: string) => {
-      if (activeGames[gameId] && activeGames[gameId].destroy) {
-        activeGames[gameId].destroy();
-      }
-
-      setActiveGames((prev) => {
-        const newGames = { ...prev };
-        delete newGames[gameId];
-        return newGames;
-      });
-    },
-    [activeGames, setActiveGames],
-  );
+  const unloadGame = useCallback((gameId: string) => {
+    console.log(`unload ${gameId}`);
+  }, []);
 
   return (
-    <GamesContext.Provider value={{ loadGame, unloadGame, activeGames }}>
+    <GamesContext.Provider value={{ loadGame, unloadGame }}>
       {children}
     </GamesContext.Provider>
   );
