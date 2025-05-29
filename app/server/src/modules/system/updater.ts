0a704e1f20d1364f34e54fa6353ee9b1838cb152
pagoru <pagoru@gmail.com>
2025-05-29 15:21:50 +0200

    feat: add autoupdate check with a cron - fix #952

---
+++ app/server/src/modules/system/updater.ts
@@ -0,0 +1,39 @@
+import { System } from "modules/system/main.ts";
+import { debug, log } from "shared/utils/log.utils.ts";
+import { update } from "@oh/utils";
+
+export const updater = () => {
+  const $update = async () => {
+    const envs = System.getEnvs();
+    const config = System.config.get();
+
+    const isDevelopment = envs.version === "development";
+
+    if (isDevelopment || !envs.upgrade) return;
+
+    console.info(
+      `Trying to update from ${envs.version} to ${config.version}...`,
+    );
+    return await update({
+      targetVersion: config.version,
+      version: envs.version,
+      repository: "openhotel/openhotel",
+      log,
+      debug,
+    });
+  };
+
+  const load = async () => {
+    const config = System.config.get();
+
+    await $update();
+
+    if (!config.autoupdate.enabled) return;
+
+    Deno.cron("updater cron", config.autoupdate.cron, $update);
+  };
+
+  return {
+    load,
+  };
+};
