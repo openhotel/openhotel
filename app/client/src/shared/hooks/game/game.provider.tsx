c1305ccf14690989fb8de20490b15db8213be242
pagoru <pagoru@gmail.com>
2025-06-03 23:18:22 +0200

    wip

---
+++ app/client/src/shared/hooks/game/game.provider.tsx
@@ -0,0 +1,58 @@
+import React, { ReactNode, useCallback, useRef } from "react";
+import { GameContext } from "./game.context";
+import { useEvents, useWindow, Event } from "@openhotel/pixi-components";
+import { Size2d } from "shared/types";
+
+type GameProps = {
+  children: ReactNode;
+};
+
+export const GameProvider: React.FunctionComponent<GameProps> = ({
+  children,
+}) => {
+  const { on } = useEvents();
+  const { getSize, getScale } = useWindow();
+
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  const $onResize = useCallback(
+    (size: Size2d) => {
+      const scale = getScale();
+      iframeRef.current.width = `${size.width * scale}px`;
+      iframeRef.current.height = `${size.height * scale}px`;
+    },
+    [getScale],
+  );
+
+  const startGame = useCallback(() => {
+    iframeRef.current = document.createElement("iframe");
+    iframeRef.current.setAttribute("src", "/phantom");
+    iframeRef.current.style.position = "absolute";
+    iframeRef.current.style.left = "0";
+    iframeRef.current.style.top = "0";
+    iframeRef.current.style.border = "0";
+    // iframeRef.current.style.opacity = ".5";
+    document.body.appendChild(iframeRef.current);
+
+    const removeOnResize = on(Event.RESIZE, $onResize);
+    $onResize(getSize());
+
+    setTimeout(() => {
+      document.body.removeChild(iframeRef.current);
+    }, 10_000);
+
+    return () => {
+      removeOnResize();
+      document.body.removeChild(iframeRef.current);
+    };
+  }, [on, $onResize, getSize]);
+
+  return (
+    <GameContext.Provider
+      value={{
+        startGame,
+      }}
+      children={children}
+    />
+  );
+};
