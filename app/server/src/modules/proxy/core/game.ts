0ae0c2fc19f7c97ddb4da76e5bbdde6a331cfa6b
pagoru <pagoru@gmail.com>
2025-06-04 20:00:20 +0200

    wip

--- app/server/src/modules/proxy/core/game.ts
+++ app/server/src/modules/proxy/core/game.ts
@@ -1,12 +1,21 @@
 import { ServerClient } from "@da/socket";
+import { Proxy } from "modules/proxy/main.ts";
 
 export const game = () => {
+  let gameMapClient: Record<string, ServerClient> = {};
+  let userMapClient: Record<string, ServerClient> = {};
+
   const guest = async (
     clientId: string,
-    state: string,
-    connectionToken: string,
+    [type, state, gameId]: string[],
     ip: string,
   ) => {
+    const config = Proxy.getConfig();
+
+    if (config.version !== "development" && state !== Proxy.getState())
+      return false;
+
+    console.log(type, clientId, state, gameId);
     return true;
   };
 
