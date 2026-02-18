1230175493dcd8cf24b53f747e9d7cfe88a97006
pablo <pagoru@gmail.com>
2026-02-18 17:38:49 +0100

    bump: vite + removed storybook - fix #1104

--- app/client/src/.storybook/__mocks__/account/fake-account.consts.ts
+++
@@ -1,4 +0,0 @@
-import { ulid } from "ulidx";
-
-export const FAKE_ACCOUNT_ID_1 = ulid();
-export const FAKE_ACCOUNT_ID_2 = ulid();
--- app/client/src/.storybook/__mocks__/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./furniture";
-export * from "./api";
-export * from "./config";
-export * from "./proxy";
-export * from "./account";
-export * from "./router";
-export * from "./item-place-preview";
-
-export * from "./fake-providers";
